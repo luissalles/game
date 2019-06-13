487afbb09d703a702c64bf00c78455b93d34b412
07619318801 <07619318801@serpro-1572782>
2019-06-13 07:35:50 -0300

    <Refatoração> refatoração dos testes

--- game/src/test/java/br/gov/serpro/game/TesteGame.java
+++ game/src/test/java/br/gov/serpro/game/TesteGame.java
@@ -2,16 +2,12 @@ package br.gov.serpro.game;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 
 public class TesteGame {
 
 	@Test
 	public void registrar10PontosEstrela() {
-		List<Ponto> pontos = new ArrayList<>();
 		Usuario usuario = new Usuario("Guerra");
 		Placar placar = new Placar(usuario);
 		placar.registrarPonto(usuario, new Ponto("Estrela", 10));
@@ -21,7 +17,6 @@ public class TesteGame {
 	
 	@Test
 	public void retornaPontosDeUmUsuario() {
-		List<Ponto> pontos = new ArrayList<>();
 		Usuario usuario = new Usuario("Guerra");
 		Placar placar = new Placar(usuario);
 		placar.registrarPonto(usuario, new Ponto("Estrela", 25));
