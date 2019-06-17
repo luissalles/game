7443a8a248837219092cd17245f56b39845d4d95
luissalles <luissalles@192.168.0.18>
2019-06-17 19:27:26 -0300

    <Testes> Construção do terceiro teste

--- game/src/test/java/br/gov/serpro/game/TesteGame.java
+++ game/src/test/java/br/gov/serpro/game/TesteGame.java
@@ -7,11 +7,15 @@ import org.junit.Test;
 public class TesteGame {
 	private Usuario usuario;
 	private Ponto pontosUsuario;
+	private String nome;
+	private Placar placar;
+	private String tipoPonto;
+	private int qtdePontos;
 	
 	@Test
 	public void registrar10PontosEstrela() {
 		usuario = new Usuario("Guerra");
-		Placar placar = new Placar(usuario);
+		placar = new Placar(usuario);
 		placar.registrarPonto(usuario, new Ponto("Estrela", 10));
 		pontosUsuario = placar.retornaPontos(usuario).get(0);
 		assertEquals(10, pontosUsuario.getQuantidadePontos());
@@ -20,10 +24,10 @@ public class TesteGame {
 	@Test
 	public void retornaPontosDeUmUsuario() {
 		usuario = new Usuario("Guerra");
-		Placar placar = new Placar(usuario);
+		placar = new Placar(usuario);
 		placar.registrarPonto(usuario, new Ponto("Estrela", 25));
 		placar.registrarPonto(usuario, new Ponto("Moeda", 20));
-		String nome = placar.getUsuario().getNome().toString();
+		nome = placar.getUsuario().getNome().toString();
 		pontosUsuario = placar.retornaPontos(usuario).get(0);
 		assertEquals("Guerra", nome);
 		assertEquals("Estrela", pontosUsuario.getTipoPonto());
@@ -33,4 +37,43 @@ public class TesteGame {
 		assertEquals(20, pontosUsuario.getQuantidadePontos());
 	}
 	
+	@Test
+	public void retornaRankingPorTipoDePonto() {
+		usuario = new Usuario("Guerra");
+		placar = new Placar(usuario);
+		placar.registrarPonto(usuario, new Ponto("Estrela", 25));
+		placar.registrarPonto(usuario, new Ponto("Moeda", 20));
+		usuario = new Usuario("Fernandes");
+		placar = new Placar(usuario);
+		placar.registrarPonto(usuario, new Ponto("Estrela", 19));
+		placar.registrarPonto(usuario, new Ponto("Moeda", 23));
+		usuario = new Usuario("Rodrigo");
+		placar = new Placar(usuario);
+		placar.registrarPonto(usuario, new Ponto("Estrela", 17));
+		placar.registrarPonto(usuario, new Ponto("Moeda", 13));
+		usuario = new Usuario("Claudio");
+		placar = new Placar(usuario);
+		placar.registrarPonto(usuario, new Ponto("Moeda", 13));
+		nome = placar.getUsuario().getNome().toString();
+		pontosUsuario = placar.retornaPontos(usuario).get(0);
+		tipoPonto = pontosUsuario.getTipoPonto();
+		qtdePontos = pontosUsuario.getQuantidadePontos();
+		assertEquals("Guerra", nome);
+		assertEquals("Estrela", tipoPonto);
+		assertEquals(25, qtdePontos);
+		nome = placar.getUsuario().getNome().toString();
+		pontosUsuario = placar.retornaPontos(usuario).get(1);
+		tipoPonto = pontosUsuario.getTipoPonto();
+		qtdePontos = pontosUsuario.getQuantidadePontos();
+		assertEquals("Fernandes", nome);
+		assertEquals("Estrela", tipoPonto);
+		assertEquals(19, qtdePontos);
+		nome = placar.getUsuario().getNome().toString();
+		pontosUsuario = placar.retornaPontos(usuario).get(2);
+		tipoPonto = pontosUsuario.getTipoPonto();
+		qtdePontos = pontosUsuario.getQuantidadePontos();
+		assertEquals("Rodrigo", nome);
+		assertEquals("Estrela", tipoPonto);
+		assertEquals(17, qtdePontos);
+	}
 }
