37e46d17def399210a7df16b05241b62871e582c
luissalles <luissalles@192.168.0.18>
2019-06-17 19:01:06 -0300

    <Refatoração> Refatoração da classe de testes

--- game/src/test/java/br/gov/serpro/game/TesteGame.java
+++ game/src/test/java/br/gov/serpro/game/TesteGame.java
@@ -5,30 +5,32 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TesteGame {
-
+	private Usuario usuario;
+	private Ponto pontosUsuario;
+	
 	@Test
 	public void registrar10PontosEstrela() {
-		Usuario usuario = new Usuario("Guerra");
+		usuario = new Usuario("Guerra");
 		Placar placar = new Placar(usuario);
 		placar.registrarPonto(usuario, new Ponto("Estrela", 10));
-		Ponto pontosUsuario = placar.retornaPontos(usuario).get(0);
+		pontosUsuario = placar.retornaPontos(usuario).get(0);
 		assertEquals(10, pontosUsuario.getQuantidadePontos());
 	}
 	
 	@Test
 	public void retornaPontosDeUmUsuario() {
-		Usuario usuario = new Usuario("Guerra");
+		usuario = new Usuario("Guerra");
 		Placar placar = new Placar(usuario);
 		placar.registrarPonto(usuario, new Ponto("Estrela", 25));
 		placar.registrarPonto(usuario, new Ponto("Moeda", 20));
 		String nome = placar.getUsuario().getNome().toString();
-		Ponto pontosUsuario = placar.retornaPontos(usuario).get(0);
+		pontosUsuario = placar.retornaPontos(usuario).get(0);
 		assertEquals("Guerra", nome);
 		assertEquals("Estrela", pontosUsuario.getTipoPonto());
 		assertEquals(25, pontosUsuario.getQuantidadePontos());
-		Ponto pontosUsuario2 = placar.retornaPontos(usuario).get(1);
-		assertEquals("Moeda", pontosUsuario2.getTipoPonto());
-		assertEquals(20, pontosUsuario2.getQuantidadePontos());
+		pontosUsuario = placar.retornaPontos(usuario).get(1);
+		assertEquals("Moeda", pontosUsuario.getTipoPonto());
+		assertEquals(20, pontosUsuario.getQuantidadePontos());
 	}
-
+	
 }
